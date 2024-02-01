0a8bee3a6c376972fffc6f7afca1118af645453f
MartinStjernkvist <mr.martin.pro@gmail.com>
2024-02-01 14:28:52 +0100

    .

--- Formler_AppD.py
+++ Formler_AppD.py
@@ -3,16 +3,18 @@ import numpy as np
 """
 Constants (keep track)
 """
-g_0 = 9.81          #
-rho_SL = 1.225      #
-t_R = 3             # runtime on takeoff
-s_TO = 500          #
-T_std = 288.15      #
-P_std = 101325      #
-gamma = 1.4         #
-R = 287             # J/kg*K
+g_0 = 9.81  #
+rho_SL = 1.225  #
+t_R = 3  # runtime on takeoff
+s_TO = 500  #
+T_std = 288.15  #
+P_std = 101325  #
+gamma = 1.4  #
+R = 287  # J/kg*K
 placeholder = 10 ** 20  # use as placeholder when creating instances of classes,
-                        # not used in calculations of the instance
+
+
+# not used in calculations of the instance
 
 class CommonFunctionalityAppD:
 
@@ -21,7 +23,7 @@ class CommonFunctionalityAppD:
 
 
 class AppendixD(CommonFunctionalityAppD):
-    def __init__(self, T_t_4_max, T_t_SLS, gamma_c, eta_c, eta_m, beta, c_pt, c_pc, T_0, M_0):
+    def __init__(self, T_t_4_max, T_t_SLS, gamma_c, eta_c, eta_m, beta, c_pt, c_pc, T_0, M_0, T_t_4, f):
         self.T_t_4_max = T_t_4_max
         self.T_t_SLS = T_t_SLS
         self.gamma_c = gamma_c
@@ -32,6 +34,8 @@ class AppendixD(CommonFunctionalityAppD):
         self.M_0 = M_0
         self.T_0 = T_0
         self.beta = beta
+        self.f = f
+        self.T_t_4 = T_t_4
         self.theta_0_break = self.calc_theta_0break(T_t_4_max, T_t_SLS)
 
     def theta_0(self):
@@ -39,17 +43,21 @@ class AppendixD(CommonFunctionalityAppD):
         Equation (D.1)
         Same as (2.52a), but with gamma_c
         """
-        theta_tau_r = (self.T_0 / T_std) * (1 + ((self.gamma_c -1) / 2) * self.M_0 ** 2)
+        theta_tau_r = (self.T_0 / T_std) * (1 + ((self.gamma_c - 1) / 2) * self.M_0 ** 2)
         return theta_tau_r
-
 
     def CompressorTotalTemperatureRatioToYield(self):
         """
         Equation (D.2)
 
-        Depends only on: turbine total temperature ratio tau_t, throttle setting: T_t_4
+        Depends only on:
+        - turbine total temperature ratio tau_t
+        - throttle setting: T_t_4
+        - flight conditions theta_0
         """
-        tau_c = 1 + self.eta_m * (1 - self.beta) * (1 + f)
+        tau_c = 1 + self.eta_m * (1 - self.beta) * ((1 + self.f) * (1 / T_std) *
+                                                    ((self.c_pt * self.T_t_4) / (self.c_pc * self.theta_0)))
+
         return tau_c
 
     def ThrottleRatio(self):
