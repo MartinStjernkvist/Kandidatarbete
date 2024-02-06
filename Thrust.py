80a53bd28e94c2957f47e1f479af63008ec9af23
MartinStjernkvist <mr.martin.pro@gmail.com>
2024-02-06 21:07:26 +0100

    .

--- Thrust.py
+++ Thrust.py
@@ -16,29 +16,6 @@ P_std = 101325  #
 gamma = 1.4  #
 R = 287.05  # J/kg*K
 
-# s_G = ? # ground roll distance
-# s_B = ? # breaking distance
-# s_TO = ? # takeoff distance
-
-"""
-theta_0_break = 1.2
-T_5 = 10 ** 20  # limiting
-stagnation_pressure_loss_combustor = 0.04
-M_SL = 1
-M_cruise = 1.7
-BPR = 3  # around 3-4
-eta_pc = 0.99
-"""
-
-"""
-T_05 = Exempel().T_0(T_5, gamma, 1)
-T_8 = Exempel().T(T_05, gamma, 1)
-
-inlet_pressure_ratio_cruise = Exempel().SPR(gamma, M_cruise)
-compressor_temperature_ratio_cruise = Exempel().STR_compressor(gamma, M_cruise, eta_pc)
-print(inlet_pressure_ratio_cruise)
-print(compressor_temperature_ratio_cruise)
-"""
 
 # exempelupg
 gamma_a = gamma
